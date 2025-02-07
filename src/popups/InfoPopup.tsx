4e5533fc0906cbf42fb1ead57ce994abe3982abf
0x5b62656e5d <dev.benjamink@gmail.com>
2025-02-07 20:07:58 +0800

    Add information popup component

---
+++ src/popups/InfoPopup.tsx
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import { IconContext } from "react-icons";
+import { RxCross2 } from "react-icons/rx";
+import { useHandleClickOutside } from "./Utils";
+
+interface InfoPopupProps {
+	showInfoPopup: boolean;
+	setShowInfoPopup: (show: boolean) => void;
+}
+
+export const InfoPopup: React.FC<InfoPopupProps> = ({ showInfoPopup, setShowInfoPopup }) => {
+	const popupRef = useRef<HTMLDivElement>(null);
+
+	useHandleClickOutside({ ref: popupRef, setPopupState: setShowInfoPopup });
+
+	return (
+		<div className={`popup ${showInfoPopup ? "show" : ""}`}>
+			<div className="popup-wrapper" ref={popupRef}>
+				<h2>What's this?</h2>
+				<p>
+					This is a simulation of the boids algorithm, developed by Craig Reynolds in
+					1986. This artificial life program simulates the flocking behavior of birds. For
+					more information, check out the{" "}
+					<a href="https://en.wikipedia.org/wiki/Boids" target="_blank">Wikipedia page</a>.
+				</p>
+				<div className="exit-form" onClick={() => setShowInfoPopup(false)}>
+					<IconContext.Provider value={{ size: "26px" }}>
+						<RxCross2 />
+					</IconContext.Provider>
+				</div>
+			</div>
+		</div>
+	);
+};
